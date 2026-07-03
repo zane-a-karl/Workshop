4ca94da660c95397d00bf8c682e0542af2b86ebd
zane-a-karl <zane.a.karl@gmail.com>
2026-07-02 22:16:27 -0700

    WS-0005: Implement first_draft of c/pargs

---
+++ c/pargs/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* #include "./pargs.h" */
+
+int main(int argc, char *argv[]) {
+    printf("/*******************\\\n");
+    printf("  Welcome to PARGS! \n");
+    printf("/*******************\\\n");
+
+    // How would I like to use it?
+    // Simply! They just give me argc and argv
+    // And I will return the next option char in the list
+    // int c = pargs(argc, argv);
+    // This isn't enough though I also need:
+    // a map b/n long <-> short options
+
+    return 0;
+
+}
---
+++ c/pargs/pargs.h
@@ -0,0 +1,6 @@
+#ifndef _PARGS_H_
+#define _PARGS_H_
+
+
+
+#endif /* _PARGS_H_ */
