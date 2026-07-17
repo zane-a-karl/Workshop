08f9de969d965a05f8764ec0a5306e72a5f15e65
zane-a-karl <zane.a.karl@gmail.com>
2026-07-16 17:02:15 -0700

    WS-0001: Make a generic POST request with libCURL

--- c/experiments/scripts/weather-alert.c
+++ c/experiments/scripts/weather-alert.c
@@ -9,7 +9,7 @@
  *  - [X] write a barebones main fn entry point
  *  - [X] try to find curl fns to set up structs
  *  - [X] make a generic GET request with curl
- *  - [ ] make a generic POST request with curl 
+ *  - [X] make a generic POST request with curl
  *  - [ ] make an api call to openweathermap's api with curl
  *  - [ ] parse the curl api call with cjson
  */
@@ -18,6 +18,7 @@
 #include <curl/easy.h>
 
 #include <stdlib.h>
+#include <stdbool.h>
 /* #include <stdio.h> */
 
 void check_curl_error(CURLcode c) {
@@ -26,27 +27,57 @@ void check_curl_error(CURLcode c) {
     }
 }
 
-void make_api_call() {
+enum HTTP_VERB { GET, POST };
+
+struct test_obj {
+    char name[32];
+    int age;
+};
+
+void make_httpbin_api_call(char *url, enum HTTP_VERB v) {
     CURLcode c;
     CURL *handle = curl_easy_init();
     c = curl_easy_setopt(handle, CURLOPT_WRITEDATA, stdout);
     check_curl_error(c);
     printf("1st setopt CURLcode c was: %d\n", c);
-    
-    char *url = malloc(256 * sizeof(*url));
-    c = sprintf(url, "https://httpbin.org/get");
-    printf("sprintf CURLcode c was: %d\n", c);
-    
+
     c = curl_easy_setopt(handle, CURLOPT_URL, url);
     check_curl_error(c);
     printf("2nd setopt CURLcode c was: %d\n", c);
-    
+
+    if (v == POST) {
+        c = curl_easy_setopt(handle, CURLOPT_POST, true);
+        check_curl_error(c);
+        printf("3rd setopt CURLcode c was: %d\n", c);
+
+        struct test_obj obj = { "Jack Aku", 42 };
+        char body[128];
+        size_t bytes =
+            sprintf(body,"name=%s&age=%d", obj.name, obj.age);
+        c = curl_easy_setopt(handle,CURLOPT_POSTFIELDS, body);
+        check_curl_error(c);
+        printf("4th setopt CURLcode c was: %d\n", c);
+
+        c = curl_easy_setopt(handle,CURLOPT_POSTFIELDSIZE,bytes);
+        check_curl_error(c);
+        printf("5th setopt CURLcode c was: %d\n", c);
+    }
+
     c = curl_easy_perform(handle);
     check_curl_error(c);
     printf("perform CURLcode c was: %d\n", c);
 }
 
+#define HTTPBIN_GET "https://httpbin.org/get"
+#define HTTPBIN_POST "https://httpbin.org/post"
+#define HTTPBIN_STATUS "https://httpbin.org/status/200"
+#define HTTPBIN_DELAY "https://httpbin.org/delay/3"
+#define HTTPBIN_JSON "https://httpbin.org/json"
+
 int main() {
-    make_api_call();
+    char *url = malloc(256 * sizeof(*url));
+    int bytes = sprintf(url, HTTPBIN_POST);
+    printf("sprintf bytes written was: %d\n", bytes);
+    make_httpbin_api_call(url, POST);
     return 0;
 }
