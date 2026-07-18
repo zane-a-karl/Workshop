d567cf7096aae564f2a50c975cb726607c8689bd
zane-a-karl <zane.a.karl@gmail.com>
2026-07-17 23:56:33 -0700

    WS-0001: Save generic POST response to a local var

--- c/experiments/scripts/weather-alert.c
+++ c/experiments/scripts/weather-alert.c
@@ -10,8 +10,10 @@
  *  - [X] try to find curl fns to set up structs
  *  - [X] make a generic GET request with curl
  *  - [X] make a generic POST request with curl
- *  - [ ] make an api call to openweathermap's api with curl
+ *  - [X] save POST response to a local variable
  *  - [ ] parse the curl api call with cjson
+ *  - [ ] make an api call to openweathermap's api with curl
+ *  - [ ] parse the openweathermap curl api call with cjson
  */
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
@@ -19,6 +21,7 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 /* #include <stdio.h> */
 
 void check_curl_error(CURLcode c) {
@@ -34,38 +37,107 @@ struct test_obj {
     int age;
 };
 
+struct response_buffer {
+    char *data;
+    size_t size_assigned;
+    size_t size_reserved;
+};
+
+size_t curl_fwrite_callback(const void *restrict ptr,
+                            size_t size,
+                            size_t nitems,
+                            void *restrict userdata) {
+
+    struct response_buffer *rb =
+        (struct response_buffer *)userdata;
+    size_t ptr_size = size * nitems;
+    size_t new_size = rb->size_assigned + ptr_size;
+
+    // Add 1 to obviate check during null termination
+    while (new_size + 1 >= rb->size_reserved) {
+        rb->data = realloc(rb->data, rb->size_reserved * 2);
+        // Anything not eq to ptr_size is an error
+        if (NULL == rb->data) { return 0; }
+        rb->size_reserved *= 2;
+    }
+
+    memcpy(rb->data + rb->size_assigned,
+                      (char *)ptr,
+                      ptr_size);
+    rb->size_assigned = new_size;
+    // Resp buf must be null terminated
+    rb->data[rb->size_assigned] = '\0';
+
+    return ptr_size;
+}
+
+#define INIT_RB_SIZE 256
+
 void make_httpbin_api_call(char *url, enum HTTP_VERB v) {
     CURLcode c;
+    char *rb_data = malloc(INIT_RB_SIZE * sizeof(*rb_data));
+    struct response_buffer rb = {rb_data, 0, INIT_RB_SIZE};
+    struct test_obj obj = { "Jack Aku", 42 };
+    char body[128]; // Must live until perform() call
     CURL *handle = curl_easy_init();
-    c = curl_easy_setopt(handle, CURLOPT_WRITEDATA, stdout);
+    c = curl_easy_setopt(handle,
+                         CURLOPT_WRITEDATA,
+                         &rb);
     check_curl_error(c);
     printf("1st setopt CURLcode c was: %d\n", c);
 
-    c = curl_easy_setopt(handle, CURLOPT_URL, url);
+    c = curl_easy_setopt(handle,
+                         CURLOPT_URL,
+                         url);
     check_curl_error(c);
     printf("2nd setopt CURLcode c was: %d\n", c);
 
     if (v == POST) {
-        c = curl_easy_setopt(handle, CURLOPT_POST, true);
+        c = curl_easy_setopt(handle,
+                             CURLOPT_POST,
+                             true);
         check_curl_error(c);
         printf("3rd setopt CURLcode c was: %d\n", c);
 
-        struct test_obj obj = { "Jack Aku", 42 };
-        char body[128];
         size_t bytes =
-            sprintf(body,"name=%s&age=%d", obj.name, obj.age);
-        c = curl_easy_setopt(handle,CURLOPT_POSTFIELDS, body);
+            snprintf(body,
+                     sizeof(body)/sizeof(*body),
+                     "name=%s&age=%d",
+                     obj.name,
+                     obj.age);
+        c = curl_easy_setopt(handle,
+                             CURLOPT_POSTFIELDS,
+                             body);
         check_curl_error(c);
         printf("4th setopt CURLcode c was: %d\n", c);
 
-        c = curl_easy_setopt(handle,CURLOPT_POSTFIELDSIZE,bytes);
+        c = curl_easy_setopt(handle,
+                             CURLOPT_POSTFIELDSIZE,
+                             bytes);
         check_curl_error(c);
         printf("5th setopt CURLcode c was: %d\n", c);
     }
 
+    c = curl_easy_setopt(handle,
+                         CURLOPT_WRITEFUNCTION,
+                         curl_fwrite_callback);
+    check_curl_error(c);
+    printf("6th setopt CURLcode c was: %d\n", c);
+
     c = curl_easy_perform(handle);
     check_curl_error(c);
     printf("perform CURLcode c was: %d\n", c);
+
+    long status_code;
+    curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &status_code);
+    printf("HTTP status: %ld\n", status_code);
+
+    // Print the response
+    printf("%s", rb.data);
+
+    // Free memory
+    free(rb.data); // free the current not the original
+    curl_easy_cleanup(handle);
 }
 
 #define HTTPBIN_GET "https://httpbin.org/get"
@@ -75,8 +147,10 @@ void make_httpbin_api_call(char *url, enum HTTP_VERB v) {
 #define HTTPBIN_JSON "https://httpbin.org/json"
 
 int main() {
-    char *url = malloc(256 * sizeof(*url));
-    int bytes = sprintf(url, HTTPBIN_POST);
+    char url[256];
+    int bytes = snprintf(url,
+                         sizeof(url)/sizeof(*url),
+                         HTTPBIN_POST);
     printf("sprintf bytes written was: %d\n", bytes);
     make_httpbin_api_call(url, POST);
     return 0;
