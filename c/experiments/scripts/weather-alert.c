2911cc7f22a0caf6b7227c2ac515b69927bd8040
zane-a-karl <zane.a.karl@gmail.com>
2026-08-15 18:33:12 -0700

    WS-0001: Make an api call to weatherapi's api with curl

--- c/experiments/scripts/weather-alert.c
+++ c/experiments/scripts/weather-alert.c
@@ -16,6 +16,8 @@
  *  - [X] setup and read test api key from env variable
  *  - [X] make an api call to openweathermap's api with curl
  *  - [X] parse the openweathermap curl api call with cjson
+ *  - [X] make an api call to weatherapi's api with curl
+ *  - [ ] parse the wapi curl api call with cjson
  */
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
@@ -65,8 +67,8 @@ size_t curl_fwrite_callback(const void *restrict ptr,
     }
 
     memcpy(rb->data + rb->size_assigned,
-                      (char *)ptr,
-                      ptr_size);
+           (char *)ptr,
+           ptr_size);
     rb->size_assigned = new_size;
     // Resp buf must be null terminated
     rb->data[rb->size_assigned] = '\0';
@@ -200,6 +202,7 @@ struct alert_input {
 
 struct alert_output {
     bool owm_will_rain;
+    bool wapi_will_rain;
 };
 
 bool make_owm_api_call(struct alert_input in) {
@@ -242,7 +245,7 @@ bool make_owm_api_call(struct alert_input in) {
     long status_code;
     curl_easy_getinfo(curl_hdl, CURLINFO_RESPONSE_CODE,
                       &status_code);
-    printf("HTTP status: %ld\n", status_code);
+    /* printf("HTTP status: %ld\n", status_code); */
 
     // Parse the JSON response
     cJSON *json_hdl = cJSON_Parse(rb.data);
@@ -321,13 +324,140 @@ bool make_owm_api_call(struct alert_input in) {
     return false;
 }
 
+bool make_wapi_api_call(struct alert_input in) {
+
+    printf("----------Begin OWM query----------\n");
+    // Initialize handles and buffers
+    CURLcode c;
+    char *rb_data = malloc(INIT_RB_SIZE * sizeof(*rb_data));
+    struct response_buffer rb = { rb_data, 0, INIT_RB_SIZE };
+    CURL *curl_hdl = curl_easy_init();
+
+    // Point a response buffer to tell curl where to write
+    c = curl_easy_setopt(curl_hdl, CURLOPT_WRITEDATA, &rb);
+    check_curl_error(c, "CURLOPT_WRITEDATA");
+
+    // Construct the url
+    char url[MAX_URL_LEN];
+    char *base = "http://api.weatherapi.com/v1";
+    char *method = "forecast.json";
+    char *city = "Irvine";
+    int days = 2;
+    char *api_key = getenv("WAPI_API_KEY");
+    // Will auto-null-terminate
+    int bytes = snprintf(url, sizeof(url)/sizeof(*url),
+                         "%s/%s?key=%s&q=%s&days=%d",
+                         base, method, api_key, city, days);
+    printf("<%d> bytes written to url: <%s>\n", bytes, url);
+    c = curl_easy_setopt(curl_hdl, CURLOPT_URL, url);
+    check_curl_error(c, "CURLOPT_URL");
+
+    // Give Curl custom callback to write to local resp buf
+    c = curl_easy_setopt(curl_hdl, CURLOPT_WRITEFUNCTION,
+                         curl_fwrite_callback);
+    check_curl_error(c, "CURLOPT_WRITEFUNCTION");
+
+    // Execute the call
+    c = curl_easy_perform(curl_hdl);
+    check_curl_error(c, "PERFORM");
+
+    // Get response status code
+    long status_code;
+    curl_easy_getinfo(curl_hdl, CURLINFO_RESPONSE_CODE,
+                      &status_code);
+    printf("HTTP status: %ld\n", status_code);
+
+    /* // Parse the JSON response */
+    /* cJSON *json_hdl = cJSON_Parse(rb.data); */
+    /* char *json_resp_str = cJSON_Print(json_hdl); */
+    /* /\* printf("response: %s\n", json_resp_str); *\/ */
+    /* cJSON_bool has_forecast = */
+    /*     cJSON_HasObjectItem(json_hdl, "forecast"); */
+    /* if (!has_forecast) { return false; } */
+    /* cJSON *forecast_hdl = cJSON_GetObjectItem(json_hdl, "forecast"); */
+    /* cJSON_bool has_forecastday = */
+    /*     cJSON_HasObjectItem(forecast_hdl, "forecastday"); */
+    /* if (!has_forecastday) { return false; }     */
+    /* cJSON *forecastday_hdl = */
+    /*     cJSON_GetObjectItem(forecast_hdl, "forecastday"); */
+    /* int forecastday_size = cJSON_GetArraySize(forecastday_hdl); */
+    /* time_t now = time(NULL); */
+    /* time_t day_in_sec = 60 * 60 * 24; */
+    /* for (int i = 0; i < forecastday_size; i++) { */
+    /*     cJSON *forecastday_item_hdl = */
+    /*         cJSON_GetArrayItem(forecastday_hdl, i); */
+    /*     cJSON_bool has_hour = */
+    /*         cJSON_HasObjectItem(forecastday_item_hdl, "hour"); */
+    /*     if (!has_hour) { return false; } */
+    /*     cJSON *dt_hdl = cJSON_GetObjectItem(list_item_hdl, "dt"); */
+    /*     cJSON_bool dt_is_number = cJSON_IsNumber(dt_hdl); */
+    /*     if (!dt_is_number) { return false; } */
+    /*     double dt = cJSON_GetNumberValue(dt_hdl); */
+
+    /*     // Skip if forecast is beyond 24 hours */
+    /*     if ((time_t)(dt) > now + day_in_sec) { continue; } */
+
+    /*     // Else check rain conditions */
+
+    /*     //// First Main Weather Designation */
+    /*     cJSON_bool has_weather = */
+    /*         cJSON_HasObjectItem(list_item_hdl, "weather"); */
+    /*     if (!has_weather) { return false; } */
+    /*     cJSON *weather_hdl = */
+    /*         cJSON_GetObjectItem(list_item_hdl, "weather"); */
+    /*     int weather_size = cJSON_GetArraySize(weather_hdl); */
+    /*     int main_rain = -1; */
+    /*     for (int j = 0; i < weather_size; i++) { */
+    /*         cJSON *weather_item_hdl = */
+    /*             cJSON_GetArrayItem(weather_hdl, j); */
+    /*         cJSON_bool has_main = */
+    /*             cJSON_HasObjectItem(weather_item_hdl, "main"); */
+    /*         if (!has_main) { return false; } */
+    /*         cJSON *main_hdl = */
+    /*             cJSON_GetObjectItem(weather_item_hdl, "main"); */
+    /*         char *main_str = cJSON_Print(main_hdl); */
+    /*         main_rain = strncmp(main_str, "Rain", 4 * sizeof(char)); */
+    /*         free(main_str); */
+    /*         if (0 == main_rain) { break; } */
+    /*     } */
+
+    /*     //// Second Probability of Precipitation (pop) */
+    /*     cJSON_bool has_pop = */
+    /*         cJSON_HasObjectItem(list_item_hdl, "pop"); */
+    /*     if (!has_pop) { return false; } */
+    /*     cJSON *pop_hdl = cJSON_GetObjectItem(list_item_hdl, "pop"); */
+    /*     cJSON_bool pop_is_number = cJSON_IsNumber(pop_hdl); */
+    /*     if (!pop_is_number) { return false; } */
+    /*     double pop = cJSON_GetNumberValue(pop_hdl); */
+
+    /*     //// Third has a 'rain' item */
+    /*     cJSON_bool has_rain = */
+    /*         cJSON_HasObjectItem(list_item_hdl, "rain"); */
+    /*     if (!has_rain) { return false; } */
+
+    /*     // Combine to check for rain */
+    /*     if (0 == main_rain && pop > 0.55 && has_rain) { */
+    /*         return true; */
+    /*     } */
+    /* } // for loop */
+
+    // Free memory
+    /* free(json_resp_str); */
+    /* cJSON_Delete(json_hdl); */
+    free(rb.data);
+    curl_easy_cleanup(curl_hdl);
+
+    printf("----------End OWM query----------\n");
+    return false;
+}
+
 int main() {
 
     /* The latitude of Irvine, California is approximately */
     /* 33.6695° N and the longitude is about -117.823° W. */
     struct alert_input in = { 33.6695, -117.823 };
     printf("Query for lat={%f}, lon={%f}\n", in.lat, in.lon);
-    struct alert_output out = { false };
+    struct alert_output out = { false, false };
     /* char url[MAX_URL_LEN]; */
     /* int bytes = snprintf(url, */
     /*                      sizeof(url)/sizeof(*url), */
@@ -336,8 +466,10 @@ int main() {
     /* make_httpbin_api_call(url, POST); */
 
     // Make openweathermap api call
-    out.owm_will_rain = make_owm_api_call(in);
-    printf("OWM says it will rain: %d\n", out.owm_will_rain);
+    /* out.owm_will_rain = make_owm_api_call(in); */
+    /* printf("OWM says it will rain: %d\n", out.owm_will_rain); */
+    out.wapi_will_rain = make_wapi_api_call(in);
+    printf("WAPI says it will rain: %d\n", out.owm_will_rain);
 
     // set -a
     // source ./.envrc
