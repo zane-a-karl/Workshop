c7e83f6ee695da556c2fe8d0c1b806ca4089d5a9
zane-a-karl <zane.a.karl@gmail.com>
2026-08-21 11:29:42 -0700

    WS-0001: Move todos into independent markdown file

---
+++ c/experiments/scripts/todos.md
@@ -0,0 +1,25 @@
+TODO:
+ [X] Compile a c program with libcurl included
+ [X] Compile a c program with cjson included
+ [X] Write a barebones main fn entry point
+ [X] Try to find curl fns to set up structs
+ [X] Make a generic GET request with curl
+ [X] Make a generic POST request with curl
+ [X] Save POST response to a local variable
+ [X] Parse the curl api call response with cjson
+ [X] Send a curl api call with a json payload
+ [X] Setup and read test api key from env variable
+ [X] Make an api call to openweathermap's api with curl
+ [X] Parse the openweathermap curl api call with cjson
+ [X] Make an api call to weatherapi's api with curl
+ [X] Parse the wapi curl api call with cjson
+ [X] Make an api call to weatherbit's api with curl
+ [X] Parse the wbit curl api call with cjson
+ [X] Return majority rain prediction
+ [X] Pull out httpbin test into a local testing file
+ [X] Consolidate curl api requests in http_get() fn
+ [X] Fix logic errors in main response computation logic
+ [X] Add test.c file with your output tests
+ [X] Capitalize all todos and remove redundant bullets
+ [X] Move todos into independent markdown file
+ [ ] Re-imagine API response traversal: make structs to hold the path
--- c/experiments/scripts/weather-alert.c
+++ c/experiments/scripts/weather-alert.c
@@ -3,29 +3,6 @@
  *       different weather APIs, averages their predictions
  *       then alerts me if there is rain in the forecast for
  *       my home in the next few days.
- * TODO:
- *  [X] Compile a c program with libcurl included
- *  [X] Compile a c program with cjson included
- *  [X] Write a barebones main fn entry point
- *  [X] Try to find curl fns to set up structs
- *  [X] Make a generic GET request with curl
- *  [X] Make a generic POST request with curl
- *  [X] Save POST response to a local variable
- *  [X] Parse the curl api call response with cjson
- *  [X] Send a curl api call with a json payload
- *  [X] Setup and read test api key from env variable
- *  [X] Make an api call to openweathermap's api with curl
- *  [X] Parse the openweathermap curl api call with cjson
- *  [X] Make an api call to weatherapi's api with curl
- *  [X] Parse the wapi curl api call with cjson
- *  [X] Make an api call to weatherbit's api with curl
- *  [X] Parse the wbit curl api call with cjson
- *  [X] Return majority rain prediction
- *  [X] Pull out httpbin test into a local testing file
- *  [X] Consolidate curl api requests in http_get() fn
- *  [X] Fix logic errors in main response computation logic
- *  [X] Add test.c file with your output tests
- *  [X] Capitalize all todos and remove redundant bullets
  */
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
